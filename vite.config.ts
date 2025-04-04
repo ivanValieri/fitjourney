7ecf0efe34df82a924642e54f5407ca89a85733f
Ivan Thadeu Valieri <prof.ivan.valieri@gmail.com>
2025-04-04 13:22:42 -0300

    fix: remover variável isDev não utilizada

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from 'path';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
-  const isDev = mode === 'development';
 
   return {
     plugins: [react()],
